85eae06bda06efc7484d50b228a47dd9352c1261
Istiak Ahmed <istiak.ahmed3@g.bracu.ac.bd>
2026-05-01 16:21:38 +0600

    string

---
+++ string.py
@@ -0,0 +1,5 @@
+s = "Programming"
+print(s[0:6])
+print(s[3:])
+print(s[:4])
+print(s[-3:])
---
+++ stringmethods.py
@@ -0,0 +1,23 @@
+# s = "  Hello World  "
+# print(s.rstrip())
+# print(s.strip())
+# print(s.lstrip())
+# print(s.upper())
+# print(s.lower())
+# print(s.rstrip())
+
+# s = "banana"
+# print(s.count("a"))
+# print(s.find("n"))
+# print(s.find("z"))
+# print(s.replace("a", "o"))
+# print(s)
+
+s = "python,java,cpp"
+lst = s.split(",")
+print(lst)
+print(len(lst))
+print(lst[1])
+
+joined = "-".join(lst)
+print(joined)
